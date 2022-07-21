32afea82dffc1a322c887128c9d5ecd8c5104c2e
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-07-21 11:29:02 -0300

    Mapeamento jpa e configuração de test

--- crud-clientes/src/main/java/com/rafaelnunes/crud/entities/Client.java
+++ crud-clientes/src/main/java/com/rafaelnunes/crud/entities/Client.java
@@ -3,12 +3,25 @@ package com.rafaelnunes.crud.entities;
 import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
 	private Integer children;
 	
