638dfe7448d539adb16d5912b193cbc328cf4f87
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-07-21 11:12:33 -0300

    Projeto crud cliente criado

---
+++ crud-clientes/src/main/java/com/rafaelnunes/crud/CrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelnunes.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientesApplication.class, args);
+	}
+
+}
